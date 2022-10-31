05d2b7254ea2d392510393a3a5256e7456e36b8e
calvin <ckodalo@gmail.com>
2022-10-31 21:47:28 +0300

    turn recursion now working

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,84 @@
+class TicTacToe
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    WIN_COMBINATIONS = [
+        [0,1,2], # Top row
+        [3,4,5], # Middle row
+        [6,7,8], # Last row
+        [0,3,6], # First Col
+        [1,4,7], # Second col
+        [2,5,8], # Third col
+        [0,4,8], # First Diagonal
+        [2,4,6]  # Second Diagonal
+    
+      ]
+
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+    
+    def input_to_index(input)
+        index = input.to_i - 1
+        index
+    end
+
+    def move(index, token="X")
+        @board[index] = token 
+    end  
+
+    def position_taken?(index)
+        if @board[index] == "X" || @board[index] == "O"
+            true
+        else
+            false
+        end 
+    end
+
+    def valid_move?(index)
+        if @board[index] && !(@board[index] == "X" || @board[index] == "O")
+            true
+        else
+            false
+        end 
+    end
+
+    def turn_count
+        @board.count("X") + @board.count("O")
+    end
+
+    def current_player
+        if @board.count("X") - @board.count("O") == 0
+            "X"
+        elsif @board.count("X") - @board.count("O") == 1
+            "O"
+        end    
+    end
+
+    def turn
+        # current_player
+        puts "choose a position between 1 and 9"
+       
+        selected_index = input_to_index(gets)
+        #position_taken?(indexfromuser)
+        if valid_move?(selected_index) 
+            move(selected_index, "#{current_player}")
+            display_board
+            
+        else
+        
+        turn
+        
+        end
+
+
+    end
+
+end  
+
